1f2a3af7a20449de56746f0724ad71e4243fde9f
SUMYAK JAIN <3456gaurav@gmail.com>
2021-08-12 10:15:17 +0530

    Update redmine_api.py

--- container-operation/redmine_api.py
+++ container-operation/redmine_api.py
@@ -2,10 +2,6 @@ import requests
 
 _get_json = lambda _url, _hdr : requests.get(_url, headers = _hdr)
 
-
-# Since failing at this point is totally developer issue (as the
-# user is not going to give the issue webpage), hence this function
-# just exit the code.
 def get_json(_url, _hdr):
     try:
         _json = _get_json(_url, _hdr)
